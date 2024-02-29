6358ea1344a6dc765d02b48024f03fa80150e77c
Akash Jangra <aj.akashjangra@gmail.com>
2024-02-29 09:23:37 +0530

    favicon and title of website updated

--- src/components/Contacts/components/ContactMethod.jsx
+++ src/components/Contacts/components/ContactMethod.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './ContactMethod.scss'
 
-const ContactMethod = () => {
+const ContactMethod = ({ name, username, link }) => {
   return (
-    <div>ContactMethod</div>
+    <div className='contact-method'>
+      <p className='name'>{`//${name}`}</p>
+      <p>{username}</p>
+      <div className='container'>
+        <p className='default name'>{`//${name}`}</p>
+        <div className='onHover'>
+          <a className='link' href={link} target='_blank'>
+            Open Link
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
